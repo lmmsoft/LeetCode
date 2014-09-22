3a8e017f65f6c03043bd7b081a577dea8b527454
lmmsoft <lmmsoft@126.com>
2014-09-22 21:51:25 +0800

    add Template and Maximum Depth of Binary Tree

---
+++ LeetCode/Maximum Depth of Binary Tree/Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,49 @@
+#include <cstdlib>
+#include <cctype>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <fstream>
+#include <numeric>
+#include <iomanip>
+#include <bitset>
+#include <list>
+#include <stdexcept>
+#include <functional>
+#include <utility>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode *root) {
+        if (root == NULL)
+        {
+            return 0;
+        }
+        else
+        {
+            return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+        }
+    }
+};
+
+int main()
+{
+    Solution s;
+}
