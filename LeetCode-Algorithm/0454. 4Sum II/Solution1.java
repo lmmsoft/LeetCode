dc1db66106cf1533f6d259d4f75914f23e1e46af
lmmsoft <lmm333@126.com>
2018-08-31 01:55:29 +0800

    Add 454.4SumII Java

--- LeetCode-Algorithm/0454. 4Sum II/Solution1.java
+++ LeetCode-Algorithm/0454. 4Sum II/Solution1.java
@@ -1,7 +1,16 @@
 package ja;
 
 import java.util.HashMap;
+import java.util.Set;
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
 class Solution {
     public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
         HashMap<Integer, Integer> h1 = new HashMap<>();
@@ -21,7 +30,13 @@ class Solution {
     private void getHash(int[] A, int[] B, HashMap<Integer, Integer> h1) {
         for (int i = 0; i < A.length; ++i) {
             for (int j = 0; j < B.length; ++j) {
-                h1.merge(A[i] + B[j], 1, (a, b) -> a + b);
+                int sum = A[i] + B[j];
+                Integer integer = h1.get(sum);
+                if (integer == null) {
+                    h1.put(sum, 1);
+                } else {
+                    h1.put(sum, integer + 1);
+                }
             }
         }
     }
