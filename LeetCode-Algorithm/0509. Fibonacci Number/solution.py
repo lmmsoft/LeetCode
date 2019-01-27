513c96423c380c5f250c18b24bfba3e5dd2ceea8
lmmsoft <lmm333@126.com>
2019-01-27 10:27:37 +0800

    ADD 0509 Fibonacci Number, 通项公式

--- LeetCode-Algorithm/0509. Fibonacci Number/solution.py
+++ LeetCode-Algorithm/0509. Fibonacci Number/solution.py
@@ -8,3 +8,10 @@ class Solution:
         for n in range(2, N + 1):
             l.append(l[n - 1] + l[n - 2])
         return l[N]
+
+    def fib2(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        return int((5 ** 0.5) * 0.2 * (((1 + 5 ** 0.5) / 2) ** N - ((1 - 5 ** 0.5) / 2) ** N))
