fb1d94201e27ef843a17aec89dabffad34fa4b95
lmmsoft <lmm333@126.com>
2019-07-12 00:11:02 +0800

    MOD: 0040, 嵌套数组去重

--- LeetCode-Algorithm/0040. Combination Sum II/0040.py
+++ LeetCode-Algorithm/0040. Combination Sum II/0040.py
@@ -8,14 +8,22 @@ class Solution:
         for num in candidates:
             for s in range(target, num - 1, -1):
                 # 不需要 and dp[s - num], 因为dp[0] 是[[]] ，可以推倒，做 [] + [num]，而 dp[1] to dp[target] 初始化都是[]，不能推倒
-                if (s - num) >= 0: # 如果加上 and dp[s - num]，会快一点，减少一些无效推倒， if [] 为False, if [[]] 为True
+                if (s - num) >= 0:  # 如果加上 and dp[s - num]，会快一点，减少一些无效推倒， if [] 为False, if [[]] 为True
                     li = [l + [num] for l in dp[s - num]]
                     dp[s] += li
         # 结果去重
-        res = [list(t) for t in set(tuple(li) for li in dp[target])]  # 这一行很精妙
+        res = [list(t) for t in set(
+            tuple(li) for li in dp[target]
+        )]  # 这一行很精妙
+
+        res2_0 = [tuple(li) for li in dp[target]]
+        res2_1 = set(res2_0)
+        res2_2 = [list(t) for t in res2_1]
+        print(res2_2)
+
         res.sort(key=dp[target].index)  # 这句话对结果在影响，是按照dp[target]里面的先后顺序，重新排序res，帮助本地assert
         print(res)
-        return res
+        return res2_2
 
 
 if __name__ == '__main__':
